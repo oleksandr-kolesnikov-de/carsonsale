699ec7b4bb6b4538d96ad49127ef2c9eab8732f4
Oleksandr <oleksandr.kolesnikov@yahoo.com>
2025-03-01 13:47:43 +0100

    rename border to border radius

--- lib/core/styles/app_border_radius.dart
+++ lib/core/styles/app_border_radius.dart
@@ -5,7 +5,7 @@
 
 import 'package:flutter/material.dart';
 
-class AppBorders {
+class AppBorderRadius {
   static const double small = 8.0;
   static const double medium = 12.0;
   static const double large = 16.0;
