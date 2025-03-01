f3e9f8bd096e5ec86766d6180eb29fc75e68f02d
Oleksandr <oleksandr.kolesnikov@yahoo.com>
2025-03-01 13:49:04 +0100

    add necessary strings for info widget

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -6,10 +6,16 @@
 class AppStrings {
   // General
   static const String appName = 'CarOnSale Challenge';
-  // Login screen
 
+  // Login
+  
   // Home screen
   static const String homeScreenTitle = 'CarOnSale Flutter Coding Challenge';
   static const String homeSearchHint = 'Enter VIN code';
   static const String homeSearchButton = 'Search';
+  static const String homeCarModel = 'Model';
+  static const String homeCarUuid = 'UUID';
+  static const String homeFeedbackPositive = 'Recommended by other users';
+  static const String homeFeedbackNegative = 'Not recommended by other users';
 }
+
