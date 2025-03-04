3d6c1f7334d3f9a02b5cfab659b80c79d014fee0
Oleksandr <oleksandr.kolesnikov@yahoo.com>
2025-03-04 18:57:23 +0100

    add properties config

--- lib/core/config/config.dart
+++ lib/core/config/config.dart
@@ -4,6 +4,12 @@
 /* ********************************************************************************************* */
 
 class Config {
+  // Retry policy
   static const int maxRetries = 3;
   static const Duration retryDelay = Duration(seconds: 2);
+  // Hive configuration
+  static const String hiveBoxName = 'cache';
+  // Server response configuration
+  static String statusCodeKey = 'statusCode';
+  static String dataKey = 'data';
 }
