476e6626085ca1f5ba28113af78b010264debc7d
Oleksandr <oleksandr.kolesnikov@yahoo.com>
2025-03-04 18:58:55 +0100

    inject cache with get it

--- lib/core/injection/core_container.dart
+++ lib/core/injection/core_container.dart
@@ -3,6 +3,7 @@
 /*                                          © 2025                                               */
 /* ********************************************************************************************* */
 
+import 'package:carsonsale/features/home/data/datasources/exchange_car_local.dart';
 import 'package:carsonsale/features/home/data/datasources/exchange_car_remote.dart';
 import 'package:carsonsale/features/home/data/datasources/exchange_car_remote_mock_impl.dart';
 import 'package:carsonsale/features/home/data/repositories/car_repository_impl.dart';
@@ -13,44 +14,45 @@ import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../features/home/data/datasources/exchange_car_local_hive_impl.dart';
 import '../../mock/server.dart';
+import '../utils/utils.dart';
 
 final core = GetIt.instance;
 
 Future configureCore() async => await init();
 
 Future<void> init() async {
-
   // Mocked server
   final serverClient = CosChallenge.httpClient;
   core.registerLazySingleton(() => serverClient);
 
   // External
- 
+
   final sharedPreferences = await SharedPreferences.getInstance();
   core.registerLazySingleton(() => sharedPreferences);
 
   // hive
-  Hive.init("");
+  Hive.init(await Utils.getPath());
 
   // BLoC
 
   core.registerFactory(() => HomeBloc(core()));
-  
 
   // UseCases
   core.registerLazySingleton(() => SearchCar(core()));
-  
+
   // Repository
   core.registerLazySingleton<CarRepository>(
-    () => CarRepositoryImpl(
-      exchangeCarRemote: core(),
-    ),
+    () => CarRepositoryImpl(core(), core()),
   );
-
   // DataSources
+  core.registerLazySingleton<ExchangeCarLocal>(
+    () => ExchangeCarLocalHiveImpl(),
+  );
   core.registerLazySingleton<ExchangeCarRemote>(
     () => ExchangeCarRemoteMockImpl(core()),
   );
   core.registerLazySingleton(() => ExchangeCarRemoteMockImpl(core()));
+  core.registerLazySingleton(() => ExchangeCarLocalHiveImpl());
 }
