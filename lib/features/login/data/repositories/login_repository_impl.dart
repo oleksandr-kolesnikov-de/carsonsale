30293f79e625db3de3f14f2d43b10a5eda3a3116
Oleksandr <oleksandr.kolesnikov@yahoo.com>
2025-03-05 21:04:55 +0100

    add data layer

---
+++ lib/features/login/data/repositories/login_repository_impl.dart
@@ -0,0 +1,25 @@
+/* ********************************************************************************************* */
+/*                                    Oleksandr Kolesnikov                                       */
+/*                                           © 2025                                              */
+/* ********************************************************************************************* */
+
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failure.dart';
+import '../../domain/repositories/login_repository.dart';
+import '../datasources/exchange_user_local.dart';
+
+class LoginRepositoryImpl implements LoginRepository {
+  final ExchangeUserLocal exchangeLocal;
+
+  LoginRepositoryImpl(this.exchangeLocal);
+
+  @override
+  Future<Either<Failure, bool>> saveUser(String userName) async {
+    final response = await exchangeLocal.saveUser(userName);
+    return response.fold(
+      (failure) => Left(failure),
+      (response) => Right(response),
+    );
+  }
+}
