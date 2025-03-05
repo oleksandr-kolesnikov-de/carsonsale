d130f97d05dd4785160523bd3183fe5c87ac6563
Oleksandr <oleksandr.kolesnikov@yahoo.com>
2025-03-05 21:04:30 +0100

    add presentational logic layer

--- lib/features/login/presentation/bloc/login_event.dart
+++ lib/features/login/presentation/bloc/login_event.dart
@@ -11,3 +11,13 @@ abstract class LoginEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class SaveUserEvent extends LoginEvent {
+  final String userName;
+  final Function onSuccess;
+
+  const SaveUserEvent({required this.userName, required this.onSuccess});
+
+  @override
+  List<Object> get props => [userName, onSuccess];
+} 
